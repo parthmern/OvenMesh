09be68134853a639565d856f6e428c4ebe31930b
parthmern <parthmern@gmail.com>
2024-04-16 15:48:52 -0400

    order model created

---
+++ server/models/Order.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const orderSchema =  new mongoose.Schema({
+
+    status: {
+        type: String,
+        enum: ['placed', 'confirmation', 'preparation', 'outForDelivery', 'delivered'],
+    },
+
+    createdAt : {
+        type: Date ,
+        default : Date.now(),
+    },
+
+    time: [{
+        type: Date, // Array of dates to store timestamps
+    }],
+
+    address : {
+        type : String ,
+    },
+
+    totalCost : {
+        type : Number ,
+    },
+
+});
+
+// Middleware to update the time array when status is updated
+orderSchema.pre('save', function (next) {
+    if (this.isModified('status')) {
+        // Push the current time to the time array
+        this.time.push(new Date());
+    }
+    next();
+});
+
+module.exports = mongoose.model('Order', orderSchema);
