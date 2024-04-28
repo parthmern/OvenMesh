2dc65acd93370ac8c033d3bbd84d89daab270c0c
parthmern <parthmern@gmail.com>
2024-04-27 21:20:53 -0400

    isAdmin added in updating and deleting pizza

--- server/routes/menuRoute.js
+++ server/routes/menuRoute.js
@@ -2,11 +2,12 @@
 
 const express = require("express");
 const { pizzaAddingController, gettingPizzaController, deletingPizzaController } = require("../controllers/menuController");
+const { isAdmin } = require("../middlewares/auth");
 
 const router = express.Router();
 
-router.post("/addingPizza", pizzaAddingController);
+router.post("/addingPizza", isAdmin, pizzaAddingController);
 router.get("/allPizza", gettingPizzaController);
-router.delete("/deletePizza", deletingPizzaController);
+router.delete("/deletePizza", isAdmin, deletingPizzaController);
 
 module.exports = router ;
