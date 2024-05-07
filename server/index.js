2f935b413a4049437f0d5710d086181ac4dbf478
parthmern <parthmern@gmail.com>
2024-05-07 07:39:51 -0400

    gpt solution

--- server/index.js
+++ server/index.js
@@ -1,82 +1,47 @@
-
-
 const express = require('express');
+const http = require('http');
+const socketIO = require("socket.io");
 const dbConnect = require('./config/dbConnect');
 const cloudinaryConnect = require('./config/cloudinaryConnect');
 
-
-
 const app = express();
-
-
-require("dotenv").config();
-
-
-// ===============================================
-
-// Socket IO
-// const { Server } = require("socket.io");
-// const {createServer} = require("http");
-// const server = createServer(app);
-
-const http = require('http');
-const { Server } = require("socket.io");
-
 const server = http.createServer(app);
-
-
-const io = new Server(server,  {
-    cors : {
-        origin : "*",
-        methods :["GET", "POST"],
-        credentials : true,
+const io = socketIO(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true,
     }
 });
 
 io.on('connection', (socket) => {
     console.log('✨🔌in server, a user connected with', socket.id);
 
-    socket.on("userJoinedWithOrderId", (data)=>{
+    socket.on("userJoinedWithOrderId", (data) => {
         console.log("in server, order id userJoinedWithOrderId event", data);
         socket.join(data);
     });
 
-    //socket.join('room1');
-
-
-    socket.on("adminJoined", (data)=>{
+    socket.on("adminJoined", (data) => {
         console.log(" ❤ 🧡 💛 💚 💙 💜  Admin joined->", data);
         socket.join(data);
-    })
-
+    });
 });
 
-module.exports = io;
+const PORT = process.env.PORT || 5000;
 
-
-// ===============================================
-
-
-const PORT = process.env.PORT || 5000 ; 
-
-server.listen(PORT, ()=>{
+server.listen(PORT, () => {
     console.log(`server running on ${PORT}`);
-})
+});
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.send("server working");
-})
-
-//=================================
-
-dbConnect();
-cloudinaryConnect();
+});
 
 //=================================
 // 💙Middlewares -------------------------------------------------
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded());
 app.use(express.json());
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
@@ -87,14 +52,11 @@ app.use(cors({
 }));
 const fileUpload = require("express-fileupload");
 app.use(
-	fileUpload({
-		useTempFiles:true,
-		tempFileDir:"/tmp",
-	})
-)
-
-
-
+    fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp",
+    })
+);
 
 //=============
 const userRoute = require("./routes/userRoute");
@@ -102,8 +64,11 @@ const menuRoute = require("./routes/menuRoute");
 const orderRoute = require("./routes/orderRoute");
 const adminRoute = require("./routes/adminRoute");
 
-
 app.use("/user", userRoute);
 app.use("/menu", menuRoute);
 app.use("/order", orderRoute);
 app.use("/admin", adminRoute);
+
+//=================================
+dbConnect();
+cloudinaryConnect();
